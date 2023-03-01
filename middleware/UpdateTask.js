e8408446cbb8e1ee85e141be41bfde07c665c4c1
shaheer924 <39200747+shaheer924@users.noreply.github.com>
2023-03-01 13:00:26 +0500

    Update UpdateTask.js

--- middleware/UpdateTask.js
+++ middleware/UpdateTask.js
@@ -1,8 +1,15 @@
 const Task = require('./../models/taskModel')
 
 const UpdateTask = async () => {
-    const task = await Task.find()
-    console.log(task)
+    const task = await Task.find({status: 'pending'})
+    for(let tasks of task){
+        if(new Date(tasks.due_date) > new Date()){
+            await Task.update({_id: tasks._id},{
+                status: 'expired'
+            })
+        }
+    }
+    console.log('Successfully updated ')
 }
 
 
